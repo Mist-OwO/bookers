f5653f4a6048c8190fda257d3d2b1030b83b2514
EC2 Default User <ec2-user@ip-172-31-42-153.ap-northeast-1.compute.internal>
2023-06-17 14:57:50 +0000

    2023/06/17

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -3,17 +3,37 @@ class BooksController < ApplicationController
     @books =Book.all
   end
 
-  def create
-    book = Book.new(book_params)
-    book.save
-    redirect_to book_path(book.id)
-  end
-
   def show
     @book = Book.find(params[:id])
   end
 
+  def create
+    @book = Book.new(book_params)
+    if @book.save
+       flash[:notice] = "Book was successfully created."
+       redirect_to book_path(@book)
+    else
+      flash.now[:alert] ="失-------------敗"
+      @books = Book.all
+      render :index
+    end
+  end
+
   def edit
+    @book = Book.find(params[:id])
+  end
+  
+  def update
+  	@book = Book.find(params[:id])
+  	@book.update(book_params)
+  	flash[:notie] = "Book was successfully updated."
+  	redirect_to book_path(@book)
+  end
+
+  def destroy
+    book = Book.find(params[:id])
+    book.destroy
+    redirect_to :action => 'index'
   end
 
   private
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root to: 'homes#top'
   get 'books/index'
-  get 'books/show'
-  get 'books/edit'
-  get 'books/new'
-  get 'top' => 'homes#top'
   post 'books' => 'books#create'
   get 'books/:id' => 'books#show', as: 'book'
+  get 'books/:id/edit' => 'books#edit', as: 'edit_book'
+  patch 'books/:id' => 'books#update', as: 'update_book'
+  delete 'books/:id' => 'books#destroy'
 end
